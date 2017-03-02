03b491a03c4afb991a422b7e32a9b3ec69c87f47
“JASON” <jrcorey84@gmail.com>
2017-03-02 08:32:50 -0600

    couldn't figure out gulp file

--- server/app.js
+++ server/app.js
@@ -1,19 +1,36 @@
 var express = require('express'),
+	path 	= require('path'),
     app     = express(),
     server  = require('http').createServer(app);
 
+  app.use(express.static(path.join(__dirname, 'public')));
 
+  app.set('views', path.join(__dirname, 'views'));
+
+  app.set('view engine', 'hbs');
 
 
   app.get('/', function(request, response){
     // request object is from the client,
     // the response is what were sending back
-    response.send('Hey I the server finally works')
+    response.send('Server be working homeslice')
+  })
+
+  app.get('/mycar', function(request, response){
+	response.json({make: 'Jeep', model: 'wrangler', year: '2007', color: 'black', murderedOut: 'Hell Yeah'})
+  })
+
+  app.get('/mycat', function(request, response){
+  	response.json({name:'Hula', type: 'awesome', personality: 'chill AF', do: 'pet when asked', doNot: 'get up in his grill'})
+  })
+
+  app.get('/handlebar', function(request, response){
+  	response.render('home')
   })
 
 
 
 // first argument is the port number
-server.listen(5000, function(){
-  console.log('server is listening, but on what port, are you reading the code?')
+server.listen(3000, function(){
+  console.log('server is listening on port 3000')
 })
